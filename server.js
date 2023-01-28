63c6d2f3b307e3f4154154e7e4f4a62897a56454
Selim-Dev <alisleem811@gmail.com>
2023-01-28 11:05:39 +0200

    mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
