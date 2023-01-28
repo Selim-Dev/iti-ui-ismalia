5c33f1fb10e709fce5e6df1b859677325d717ebd
Selim-Dev <alisleem811@gmail.com>
2023-01-28 12:34:56 +0200

    adding express json from karim

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const port = 4000
 const cors = require(' cors');
 const mongoose = require('mongoose');
 
+
+app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('pull request!')
+  res.send('Karim  request!')
 })
 
 mongoose.connect('xmongodb://127.0.0.1:27017/test');
