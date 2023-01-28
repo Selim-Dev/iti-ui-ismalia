22358d46d82ba0481403a1de5384deee1a416e68
Selim-Dev <alisleem811@gmail.com>
2023-01-28 11:37:11 +0200

    add routes to post  model

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const mongoose = require('mongoose');
+const port = 5000;
+const cors = require(' cors');
+const mongoose = require('montestgoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
